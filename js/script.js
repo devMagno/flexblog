1501c5cacff143975d235256ecbd2ec90bfc69db
Guilherme Magno <guilhermertfmagno@gmail.com>
2020-11-02 16:31:02 -0300

    Configura scroll suave para links internos

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const menuItems = document.querySelectorAll('.menu nav ul li a[href^="#"]')
+
+menuItems.forEach(item => {
+  item.addEventListener('click', scrollToIdOnClick)
+})
+
+function scrollToIdOnClick(event) {
+  event.preventDefault()
+  const element = event.target
+  const id = element.getAttribute('href')
+  const to = document.querySelector(id).offsetTop
+
+  window.scroll({
+    top: to,
+    behavior: "smooth",
+  })
+}
